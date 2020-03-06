8ad5055770d7f82ca90e327d522dbb94740714d0
Alex <suarez9093@hotmail.com>
2020-03-05 22:21:06 -0700

    commented out the api folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
+// Dependencies
 const router = require("express").Router();
+// require the defined weather routes
 const weatherRoutes = require("./weatherRoutes");
 
-// Weather routes /
+// Weather routes use /weather for the endpoint
 router.use("/weather", weatherRoutes);
 
 module.exports = router;
